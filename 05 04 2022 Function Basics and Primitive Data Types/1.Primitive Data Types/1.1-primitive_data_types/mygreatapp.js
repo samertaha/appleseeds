848a64aeee11f3409642408e266564babc1e4795
Samer taha <alrazy1@gmail.com>
2022-04-05 11:45:29 +0300

    primitive_data_types

---
+++ 05 04 2022 Function Basics and Primitive Data Types/1.Primitive Data Types/1.1-primitive_data_types/mygreatapp.js
@@ -0,0 +1,12 @@
+const stringy = 'samer';
+const numeric = 10;
+const myCar = {
+  make: 'Ford',
+  model: 'Mustang',
+  year: 1996,
+};
+const imgood = true;
+const nulled = null;
+const udefinedconst = undefined;
+const _counter = Symbol('counter');
+console.log(stringy, numeric, myCar);

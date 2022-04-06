f8d23877709c250ff146f3d30a2e73f7a124f9ba
Samer taha <alrazy1@gmail.com>
2022-04-06 11:41:20 +0300

    final

---
+++ 06 04 2022 Functions Operations and If Statements/Different Ways to Declare Functions/3.2 - delaring functions part_2/function-declaration-part-2.js
@@ -0,0 +1,43 @@
+/*
+    JavaScript - Declaring Functions
+
+    The following exercise contains the following subjects:
+        * functions
+    
+    Instructions
+        * Please reformat the following function expressions to IIFE functions.
+        * Please reformat the following function declarations in two ways, explicit return and implicit return functions.
+
+    Submit the file to Hive
+
+*/
+
+// From function declarations to explicit and implicit return functions (one of each).
+function welcome() {
+  let welcome = 'Welcome to Appleseeds Bootcamp!';
+  return welcome;
+}
+const welcome1 = () => 'Welcome to Appleseeds Bootcamp!';
+
+function power(a) {
+  let myNumber = a;
+  let result = Math.pow(myNumber, 2);
+  return result;
+}
+const power1 = (a) => {
+  let myNumber = a;
+  let result = Math.pow(myNumber, 2);
+  return result;
+};
+// From function expressions to IIFE functions.
+const squareRoot = (a) => Math.sqrt(a);
+((a) => Math.sqrt(a))();
+const randomNumbers = (a, b) => Math.random() * (a - b) + b;
+
+((a, b) => {
+  Math.random() * (a - b) + b;
+})();
+
+const randomNumbers1 = (function (a, b) {
+  return Math.random() * (a - b) + b;
+})();

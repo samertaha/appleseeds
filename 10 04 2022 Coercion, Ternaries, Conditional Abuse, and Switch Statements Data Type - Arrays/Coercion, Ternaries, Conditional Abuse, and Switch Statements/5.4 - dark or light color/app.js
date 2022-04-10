47a2594782eb443279c74947ec19aaeebec1cfe9
Samer taha <alrazy1@gmail.com>
2022-04-10 12:58:12 +0300

    finnished

---
+++ 10 04 2022 Coercion, Ternaries, Conditional Abuse, and Switch Statements Data Type - Arrays/Coercion, Ternaries, Conditional Abuse, and Switch Statements/5.4 - dark or light color/app.js
@@ -0,0 +1,18 @@
+lightDarkUnknown = (color) => {
+  switch (color) {
+    case 'yellow':
+    case 'pink':
+    case 'orange':
+      return 'light color';
+    case 'blue':
+    case 'purple':
+    case 'brown':
+      return 'dark color';
+    default:
+      return 'Unknown color';
+  }
+};
+
+console.log(lightDarkUnknown('yellow'));
+console.log(lightDarkUnknown('blue'));
+console.log(lightDarkUnknown('fkgj'));

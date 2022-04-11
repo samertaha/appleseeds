8cd24851209306658bcb260228893b9c29b01586
Samer taha <alrazy1@gmail.com>
2022-04-11 16:46:32 +0300

    ok good

--- 11 04 2022 Data Type - Objects and Array Methods/Data Type - Objects/8.5 - object iteration/app.js
+++ 11 04 2022 Data Type - Objects and Array Methods/Data Type - Objects/8.5 - object iteration/app.js
@@ -1,10 +1,10 @@
 // Create a function that receives 1 argument, an object and returns a new object with the properties and values swapped
 const swap = (obj) => {
-  let ret = {};
-  for (var key in obj) {
-    ret[obj[key]] = key;
+  let res = {};
+  for (let key in obj) {
+    res[obj[key]] = key;
   }
-  return ret;
+  return res;
 };
 
 console.log(swap({ name: 'samer', job: 'fullstack' }));

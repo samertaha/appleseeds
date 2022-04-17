184af5eade7f57ed712567d8285d72e0d87081ec
Samer taha <alrazy1@gmail.com>
2022-04-17 12:02:40 +0300

    OK

--- 17 04 2022 Sort and Reduce Method/Sort/13.2 - sort strings/app.js
+++ 17 04 2022 Sort and Reduce Method/Sort/13.2 - sort strings/app.js
@@ -2,3 +2,24 @@ const foods = ['falafel', 'sabich', 'hummus', 'pizza with extra pineapple'];
 
 console.log(foods.reverse());
 console.log(foods.sort());
+
+// let sortme = ["a","b","A","B"];
+// sortme.sort((a,b)=>b.localeCompare(a))
+// console.log(sortme) // ["B", "b", "A", "a"]
+// sortme.reverse() // ["a", "A", "b", "B"]
+
+const foodsWithUpperCase = [
+  'falafel',
+  'Sabich',
+  'hummus',
+  'pizza with extra pineapple',
+];
+
+foodsWithUpperCase.sort((a, b) => a.localeCompare(b));
+console.log(foodsWithUpperCase);
+foodsWithUpperCase.sort((a, b) => b.localeCompare(a));
+console.log(foodsWithUpperCase);
+
+const words = ['apple', 'supercalifragilisticexpialidocious', 'hi', 'zoo'];
+
+console.log(words.sort((a, b) => a.length - b.length));

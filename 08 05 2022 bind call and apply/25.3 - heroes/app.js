221b736b78757f45b10bfc374d24565e93e67610
Samer taha <alrazy1@gmail.com>
2022-05-08 14:38:37 +0300

    OK

---
+++ 08 05 2022 bind call and apply/25.3 - heroes/app.js
@@ -0,0 +1,10 @@
+const wonderWoman = { name: 'Diana Prince' };
+const batman = { name: 'Bruce Wayne' };
+const superHeroes = [wonderWoman, batman];
+function printName() {
+  console.log(`my name is ${this.name}`);
+}
+function printHeroes(heroes, printFunc) {
+  heroes.forEach((hero) => printFunc.apply(hero));
+}
+printHeroes(superHeroes, printName);
